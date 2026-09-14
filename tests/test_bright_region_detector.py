21448b0b9dff003e1be7dc79def083df819b9a28
kadoka <tomiya3499@gmail.com>
2026-09-14 23:02:11 +0900

    Test image detector common element output

--- tests/test_bright_region_detector.py
+++ tests/test_bright_region_detector.py
@@ -1,21 +1,23 @@
 import unittest
 
 from ai_game_player.bright_region_detector import BrightRegionDetector
+from ai_game_player.models import DetectedElement
 from ai_game_player.screen_capture import ScreenFrame
 
 
 class BrightRegionDetectorTest(unittest.TestCase):
-    def test_detects_bright_rectangle_as_center_candidate(self):
+    def test_detects_bright_rectangle_as_detected_element(self):
         pixels = bytearray(6 * 5 * 4)
         for y in range(1, 4):
             for x in range(2, 5):
                 offset = (y * 6 + x) * 4
                 pixels[offset : offset + 4] = bytes([255, 255, 255, 255])
-        candidates = BrightRegionDetector().detect(ScreenFrame(6, 5, bytes(pixels)))
-        self.assertEqual(len(candidates), 1)
-        self.assertEqual((candidates[0].x, candidates[0].y), (3, 2))
-        self.assertEqual(candidates[0].bbox, (2, 1, 3, 3))
-        self.assertGreaterEqual(candidates[0].confidence, 0.5)
+        elements = BrightRegionDetector().detect(ScreenFrame(6, 5, bytes(pixels)))
+        self.assertEqual(len(elements), 1)
+        self.assertIsInstance(elements[0], DetectedElement)
+        self.assertEqual(elements[0].bbox, (2, 1, 3, 3))
+        self.assertEqual(elements[0].source, "bright_region")
+        self.assertGreaterEqual(elements[0].confidence, 0.5)
 
     def test_ignores_small_bright_noise(self):
         pixels = bytearray(4 * 4 * 4)
